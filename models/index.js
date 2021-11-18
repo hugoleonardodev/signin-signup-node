2131ee6a1c709af15f6551df2ea6436a0c943b0e
Hugo Leonardo <hugoleonardo.dev@gmail.com>
2021-11-18 17:03:26 -0300

    :construction: running app

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize')
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
     host: config.HOST,
     dialect: config.dialect,
-    operatorsAliases: false,
+    // operatorsAliases: false,
 
     pool: {
         max: config.pool.max,
@@ -19,8 +19,8 @@ const db = {}
 db.Sequelize = Sequelize
 db.sequelize = sequelize
 
-db.user = require('../models/user.model.js')(sequelize, Sequelize)
-db.role = require('../models/role.model.js')(sequelize, Sequelize)
+db.user = require('./user.model.js')(sequelize, Sequelize)
+db.role = require('./role.model.js')(sequelize, Sequelize)
 
 db.role.belongsToMany(db.user, {
     through: 'user_roles',
--- server.js
+++ server.js
@@ -16,8 +16,9 @@ app.use(bodyParser.json())
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const db = require('./app/models')
+const db = require('./models')
 const Role = db.role
+const User = db.user
 
 db.sequelize.sync({ force: true }).then(() => {
     console.log('Drop and Resync Db')
@@ -30,11 +31,12 @@ app.get('/', (req, res) => {
 })
 
 // routes
-require('./app/routes/auth.routes')(app)
-require('./app/routes/user.routes')(app)
+require('./routes/auth.routes')(app)
+require('./routes/user.routes')(app)
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
 })
@@ -53,5 +55,29 @@ function initial() {
     Role.create({
         id: 3,
         name: 'admin',
+    })
+
+    User.create({
+        id: 1,
+        username: 'admin',
+        password: 'admin',
+        email: 'admin@app.com',
+        roles: ['admin', 'moderator', 'user'],
+    })
+
+    User.create({
+        id: 2,
+        username: 'moderator',
+        password: 'moderator',
+        email: 'moderator@app.com',
+        roles: ['moderator', 'user'],
+    })
+
+    User.create({
+        id: 3,
+        username: 'user',
+        password: 'user',
+        email: 'user@app.com',
+        roles: ['user'],
     })
 }
